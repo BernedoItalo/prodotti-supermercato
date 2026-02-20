e38206da40db056c68ebe49d23bc52bfd9603a12
BernedoItalo <rodriguezbernedo.italomanuel.studente@itispaleocapa.it>
2026-02-20 10:23:27 +0100

    Add files via upload

---
+++ supermercato 1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+
+
+//nome prodoto,prezzo,categoria,
+
+
+struct  supermercato 
+{
+    string nome_prodoto;
+    string categoria;
+    int prezzo;
+	
+	
+};
+
+
+int main()
+{
+	
+	
+	return 0;
+}
